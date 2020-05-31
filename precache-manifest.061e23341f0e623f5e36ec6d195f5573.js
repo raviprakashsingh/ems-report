3e55c70cfa8d582aa1531ca1af39bbe30d581584
Ravi Prakash Singh <Ravip@cloudthing.com>
2020-05-31 22:00:06 +0530

    Updates

---
+++ precache-manifest.061e23341f0e623f5e36ec6d195f5573.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9ce93a05e3fb414ebf1d0ef76e91ba",
+    "url": "/ems-report/index.html"
+  },
+  {
+    "revision": "4d9d2e5bcf97a62bfdcf",
+    "url": "/ems-report/static/css/main.2f5972b4.chunk.css"
+  },
+  {
+    "revision": "defcee446e8c41557087",
+    "url": "/ems-report/static/js/2.4941df42.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/ems-report/static/js/2.4941df42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9d2e5bcf97a62bfdcf",
+    "url": "/ems-report/static/js/main.563eeb6a.chunk.js"
+  },
+  {
+    "revision": "c0b7aba8a01b48fafc5d",
+    "url": "/ems-report/static/js/runtime-main.2ba2e7b3.js"
+  },
+  {
+    "revision": "abf69411e4d651a28c2591c677e241c0",
+    "url": "/ems-report/static/media/power-station.abf69411.jpg"
+  },
+  {
+    "revision": "c74a4a94116341d488ce9fdd541eda80",
+    "url": "/ems-report/static/media/smart-fi.c74a4a94.png"
+  },
+  {
+    "revision": "f4ab16780953f0f167402c3c92800e14",
+    "url": "/ems-report/static/media/venlite-logo.f4ab1678.png"
+  }
+]);
